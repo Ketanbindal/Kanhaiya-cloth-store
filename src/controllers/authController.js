0264f3cdd39ba2faba4ae60f0d6821cb127845d0
Ketan Bindal <ketan.bindal@digitalglyde.com>
2025-02-18 17:52:42 +0530

    made changes in the authcontroller.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,4 +16,13 @@ exports.Signup = async (req, res)=>
     {
         return res.status(400).json({ errors: errors.array() });
     }
-}
+    try{
+        const { username, email, Password} = req.body;
+        const hashpass = await bcrypt.hash(Password, 10);
+        const user = new User({username, email, Password : hashpass});
+        await user.save();
+        res.status(201).json({message : 'User Registered Sucessfully'});
+    }catch(e){
+        res.status(500).json({error:error.message})
+    }
+};
